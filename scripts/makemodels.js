9879ce7a350a1769273a0b3f89740664f07381c5
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-02 19:50:09 +0200

    Update makemodels.js

--- scripts/makemodels.js
+++ scripts/makemodels.js
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-import { find } from "../utils/finder.js"
-import { arrayColumn, arrayDiff, encode, group } from "../utils/functions.js"
+import { arrayColumn, chunk, encode, group, isVideo } from "../utils/functions.js"
 
 const rootDir = "/media/images"
 
@@ -8,11 +7,6 @@ const prisma = new PrismaClient()
 
 const DBfiles = await prisma.file.findMany()
 console.log("Found: ",  DBfiles.length)
-
-// /media/images/{collection}/{user}/a.jpg
-// /media/images/{collection}/{user}/subdir/b.jpg
-
-
 
 const models = {
     collection: [],
@@ -36,45 +30,68 @@ collections.forEach(({ directory: collectionPath, items: collectionFiles }) => {
     models.collection.push(collection)
 
     getChildren(collectionPath, collectionFiles).forEach(({ directory: userPath, items: userFiles }) => {
-
+        
         const user = {
             id: encode(userPath),
             name: userPath.split("/").pop(),
             path: userPath.replace(collectionPath, ""),
             ownerId: collection.id
         }
-
-        models.user.push(user)
-
         
+        models.user.push(user)
+        
+        const fileGroups = chunk(userFiles, 10)
+
+        fileGroups.forEach((fileGroup, i) => {
+            
+            const postPath = `${userPath}:${i}`
+
+            const post = {
+                id: encode(postPath),
+                path: postPath.replace(userPath, ""),
+                ownerId: user.id
+            }
+
+            models.post.push(post)
+
+            fileGroup.forEach(({ path: mediaPath }) => {
+
+                const video = isVideo(mediaPath)
+
+                const media = {
+                    id: encode(mediaPath),
+                    ownerId: post.id,
+                    filePath: mediaPath,
+                    type: video ? "VIDEO" : "IMAGE"
+                }
+
+
+                if (!post.thumb) post.thumb = video ? `t:${media.videoId}` : `m:${media.imageId}`
+
+                models.media.push(media)
+
+            })
+            
+            if (!user.picture) user.picture = post.thumb
+
+        })
 
 
     })
 
 })
 
-console.log(models.collection.shift())
 
-// DBfiles.slice(0, 1).forEach(({ path }) => {
-
-//     const collection = path.replace(rootDir, "").split("/").shift()
-
-//     const collectionPath = rootDir + collection
-
-//     console.log(collection)
-
-// })
-
-
-// const newFiles = arrayDiff(files, DBids)
-// console.log("New: ",  newFiles.length)
-
-// if (newFiles.length) {
-
-//     const pr = await prisma.file.createMany({
-//         data: newFiles.map(item => ({path: item }))
-//     })
-
-//     console.log(pr)
+for (const model in models) {
     
-// }
+    const DBmodels = await prisma[model].findMany({select: {id: true}})
+    const DBmodelsIds = arrayColumn(DBmodels, "id")
+
+    const newModels = models[model].filter(({ id }) => !DBmodelsIds.includes(id))
+    console.log(`New ${model}:`, newModels.length)
+
+    await prisma[model].createMany({
+        data: newModels 
+    })
+
+}
