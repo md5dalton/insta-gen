00de529440ab1cf3a6fb46edd6b8e5892e64c050
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-11 16:30:06 +0200

    Update route.js

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -1,11 +1,25 @@
 import { getPosts, getVideos } from "@/actions/random"
+import { getUser } from "@/actions/user"
 
-export async function GET(req, { params: { page } }) {
+export async function GET(req, { params: { id } }) {
+    
+    const DBuser = await getUser(id)
+    
+    if (!DBuser) return new Response("User not found", { status: 404 })
 
+    const { posts, ...user } = DBuser
+
+    console.log(DBuser)
+    
     return Response.json({
-        media: media,
-        page,
-        end: false
+        user: {
+            ...user,
+            stats: [
+                {name: "posts", value: posts.length},
+                {name: "followers", value: "347k"},
+                {name: "following", value: "143"}
+            ]
+        }
     })
 
 }
