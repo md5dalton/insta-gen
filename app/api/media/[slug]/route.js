7dc07df480eeae1a8a22fca10e0f4d3ba56b3ce4
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-11 16:30:04 +0200

    Update route.js

--- app/api/media/[slug]/route.js
+++ app/api/media/[slug]/route.js
@@ -3,40 +3,21 @@ import fs from "fs"
 
 export async function GET(request, { params: { slug } }) {
 
-    // const media = await prisma.media.findUnique({
-    //     where: {
-    //         id: slug
-    //     },
-    //     include: {
-    //         owner: {
-    //             include: {
-    //                 owner: {
-    //                     include: {
-    //                         owner: {
-    //                             include: {
-    //                                 owner: true
-    //                             }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // })
+    const media = await prisma.media.findUnique({
+        where: {
+            id: slug
+        },
+        select: {
+            path: true
+        }
+    })
 
-    // if (media) {
-        // const post = media.owner
-        // const user = post.owner
-        // const collection = user.owner
-        // const rootCollection = collection.owner
-
-        // const imagePath = process.env.MEDIA_ROOT + rootCollection.path + collection.path + user.path + post.path.split("<").shift() + media.path
-        const imagePath = "/media/1.jpg" 
-        return fs.existsSync(imagePath) ? new Response(fs.readFileSync(imagePath), {
-                headers: {"Content-Type": "image/jpeg"},
-                status: 200
-        }) : new Response('Image not found', { status: 404 })
-
-    // } else return new Response('Media not found', { status: 404 })
+    if (!media) return new Response('Media not found', { status: 404 })
+    if (fs.existsSync(media.path)) return new Response('File not found', { status: 404 })
+    
+    return new Response(fs.readFileSync(media.path), {
+        headers: {"Content-Type": "image/jpeg"},
+        status: 200
+    })
 
 }
