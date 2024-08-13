c579b9a6353f7aba3427e9b4d9ac344f1e96b226
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-13 15:00:23 +0200

    Update route.js

--- app/api/feed/[page]/route.js
+++ app/api/feed/[page]/route.js
@@ -1,40 +1,11 @@
-import { getPosts, getVideos } from "@/actions/random"
+import { getPosts } from "@/actions/feed"
 
 export async function GET(req, { params: { page } }) {
 
-    const media = []
-    const pageSize = 8
-
-    const DBposts = await getPosts(pageSize)
-    const DBvideos = await getVideos(2)
-
-    const posts = DBposts.map(({ id, thumb, media }) => {
-
-        const videos = media.filter(({ type }) => type == "VIDEO")
-
-        return ({
-            id,
-            thumb,
-            hasMany: media.length > 1,
-            hasReel: videos.length ? true : false
-        })
-
-    })
-
-
-    for (let i = 0; i < posts.length; i += 2) media.push(posts.slice(i, i + 2))
-
-    if (DBvideos.length > 1) {
-
-        media.splice(2, 0,
-            [{reel: true, ...DBvideos.pop()}],
-            [{reel: true, ...DBvideos.pop()}]
-        )
-
-    }
+    const DBposts = await getPosts()
     
     return Response.json({
-        media: media,
+        media: DBposts,
         page,
         end: false
     })
