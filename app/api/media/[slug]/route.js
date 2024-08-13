859c5ce5e1bf918765c72de6fc14a67e96789654
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-08-13 15:00:25 +0200

    Update route.js

--- app/api/media/[slug]/route.js
+++ app/api/media/[slug]/route.js
@@ -1,16 +1,9 @@
-import prisma from "@/utils/prisma"
+import { getMedia } from "@/actions/media"
 import fs from "fs"
 
 export async function GET(request, { params: { slug } }) {
 
-    const media = await prisma.media.findUnique({
-        where: {
-            id: slug
-        },
-        select: {
-            path: true
-        }
-    })
+    const media = await getMedia(slug)
 
     if (!media) return new Response('Media not found', { status: 404 })
     if (fs.existsSync(media.path)) return new Response('File not found', { status: 404 })
