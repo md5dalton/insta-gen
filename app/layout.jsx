7ce9b5d07cbac51e709a552111d16b9b2a60f91a
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-09-21 17:02:18 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,12 @@
 import "./globals.css"
 
 export const metadata = {
-  title: "Insta generator",
-  description: "Insta Generator",
+    title: "Insta generator",
+    description: "Insta Generator",
 }
 
-export default function RootLayout({ children }) {
-  return (
+export default ({ children }) => (
     <html lang="en">
-      <body>{children}</body>
+        <body>{children}</body>
     </html>
-  )
-}
+)
