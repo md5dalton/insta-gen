59bad7faea439f538184dd3821e66d5fbf3403b3
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-01-05 09:08:05 +0200

    Update route.js

--- app/api/media/[slug]/route.js
+++ app/api/media/[slug]/route.js
@@ -1,16 +1,15 @@
 import { getMedia } from "@/actions/media"
-import { getMediaDir, getRealpath } from "@/utils/functions"
+import { getRealpath } from "@/utils/functions"
 import fs from "fs"
 
 export async function GET(req, { params: { slug } }) {
-
     
     const media = await getMedia(slug)
     
     const path = getRealpath(media.path)
 
     if (!media) return new Response('Media not found', { status: 404 })
-    if (fs.existsSync(path)) return new Response('File not found', { status: 404 })
+    if (!fs.existsSync(path)) return new Response('File not found', { status: 404 })
     
     return new Response(fs.readFileSync(path), {
         headers: {"Content-Type": "image/jpeg"},
