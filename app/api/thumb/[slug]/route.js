e94f3c78c267449b5330724923dfb8013ec6f547
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-20 09:28:53 +0200

    Update route.js

--- app/api/thumb/[slug]/route.js
+++ app/api/thumb/[slug]/route.js
@@ -1,9 +1,9 @@
+import { getThumsDir } from "@/utils/functions"
 import fs from "fs"
 
 export async function GET(req, { params: { slug } }) {
-
-    // const imagePath = `${process.env.THUMB_ROOT}/${slug}.jpg`
-    const imagePath = `/media/1.jpg`
+    
+    const imagePath = `${getThumsDir()}/1.jpg`
     
     return fs.existsSync(imagePath) ? new Response(fs.readFileSync(imagePath), {
             headers: {"Content-Type": "image/jpeg"},
