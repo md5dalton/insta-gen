77a6288f1d20372cc6c455212e2cbdd373419839
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-26 08:07:40 +0200

    Update makemodels.js

--- scripts/makemodels.js
+++ scripts/makemodels.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-import { arrayColumn, chunk, encode, getMediaDir, group, isVideo } from "../utils/functions.js"
+import { arrayColumn, chunk, encode, getMediaDir, group, isVideo, sortChunk } from "../utils/functions.js"
 
 const rootDir = "/media"
 
@@ -148,8 +148,9 @@ for (const userId in users) {
     const user = users[userId]
     const { id, path, media } = user
     
-    const fileGroups = chunk(media, 10)
-
+    // const fileGroups = chunk(media, 10)
+    const fileGroups = sortChunk(media, 10, ({ isVideo }) => isVideo)
+    
     fileGroups.forEach((fileGroup, i) => {
         
         const modelPost = new Post(user, `:${i}`)
@@ -182,7 +183,7 @@ const models = {
     media: medias,
     mediaTag: mediaTags
 }
-// console.table(posts)
+
 for (const model in models) {
     
     const DBmodels = await prisma[model].findMany({select: {id: true}})
@@ -196,70 +197,3 @@ for (const model in models) {
     })
 
 }
-// const getChildren = (dir, items) => group(items, ({ path }) => dir + "/" + path.replace(dir + "/", "").split("/").shift())
-
-// const collections = getChildren(rootDir, DBfiles)
-// console.log(DBfiles.pop())
-
-// collections.forEach(({ directory: collectionPath, items: collectionFiles }) => {
-
-//     const collection = {
-//         id: encode(collectionPath),
-//         name: collectionPath.split("/").pop(),
-//         path: collectionPath.replace(rootDir, "")
-//     }
-
-//     models.collection.push(collection)
-
-//     getChildren(collectionPath, collectionFiles).forEach(({ directory: userPath, items: userFiles }) => {
-        
-//         const user = {
-//             id: encode(userPath),
-//             name: userPath.split("/").pop(),
-//             path: userPath.replace(collectionPath, ""),
-//             ownerId: collection.id
-//         }
-        
-//         models.user.push(user)
-        
-//         const fileGroups = chunk(userFiles, 10)
-
-//         fileGroups.forEach((fileGroup, i) => {
-            
-//             const postPath = `${userPath}:${i}`
-
-//             const post = {
-//                 id: encode(postPath),
-//                 path: postPath.replace(userPath, ""),
-//                 ownerId: user.id
-//             }
-
-//             models.post.push(post)
-
-//             fileGroup.forEach(({ path: mediaPath }) => {
-
-//                 const video = isVideo(mediaPath)
-
-//                 const media = {
-//                     id: encode(mediaPath),
-//                     path: mediaPath,
-//                     ownerId: post.id,
-//                     filePath: mediaPath,
-//                     type: video ? "VIDEO" : "IMAGE"
-//                 }
-
-
-//                 if (!post.thumb) post.thumb = video ? `t:${media.id}` : `m:${media.id}`
-
-//                 models.media.push(media)
-
-//             })
-            
-//             if (!user.picture) user.picture = post.thumb
-
-//         })
-
-
-//     })
-
-// })
