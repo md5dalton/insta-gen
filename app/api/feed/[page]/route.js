8e92c1f099d299cd4d576aa2190a954db53ac960
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-30 17:01:28 +0200

    Update route.js

--- app/api/feed/[page]/route.js
+++ app/api/feed/[page]/route.js
@@ -1,11 +1,26 @@
 import { getPosts } from "@/actions/feed"
+import { metadata } from "@/app/layout"
 
 export async function GET(req, { params: { page } }) {
 
     const DBposts = await getPosts()
 
+    const media = DBposts.map(({ media, ...rest }) => {
+        
+        const { metadata: { height, width } } = media.reduce((prev, current) => (prev.metadata.height > current.metadata.height) ? prev : current)
+  
+        // const height = Math.max(...media.map(({ metadata: { height } }) => height))
+
+        return ({
+            ...rest,
+            // height,
+            aspect: height/width,
+            media
+        })
+    })
+
     return Response.json({
-        media: DBposts,
+        media: media,
         page,
         end: false
     })
