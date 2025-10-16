74496213c6fdec8bf0caa61e13f04eae4d230968
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-10-16 23:57:22 +0200

    Update prisma.js

--- utils/prisma.js
+++ utils/prisma.js
@@ -1,7 +1,11 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 import prismaRandom from "prisma-extension-random"
 
-const prisma = globalThis.prisma || new PrismaClient().$extends(prismaRandom())
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = globalThis.prisma || new PrismaClient({ adapter }).$extends(prismaRandom())
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
 
