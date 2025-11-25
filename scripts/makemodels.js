d4ba9d7dd387a70fbea0610a58cab7d26f9da6e3
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-11-25 19:13:41 +0200

    Update makemodels.js

--- scripts/makemodels.js
+++ scripts/makemodels.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client"
-import { arrayColumn, chunk, encode, getMediaDir, group, isVideo, sortChunk } from "../utils/functions.js"
+import { arrayColumn, encode, isVideo, sortChunk } from "../utils/functions.js"
 
 const prisma = new PrismaClient()
 
