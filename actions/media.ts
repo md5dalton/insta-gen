81a441c6615fc6af7228833bd0611b423288db41
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-11-27 20:24:31 +0200

    Create media.ts

---
+++ actions/media.ts
@@ -0,0 +1,33 @@
+import prisma from "@/utils/prisma"
+
+type Media = {
+    path: string
+}
+
+type RandomMedia = {
+    id: string
+    isVideo: boolean
+    owner: {
+        id: string
+        name: string
+        picture: string | null
+    }
+}
+export const getMedia = async (slug: string): Promise<Media | null> => await prisma.media.findUnique({
+    where: { id: slug },
+    select: { path: true }
+})
+
+export const getRandom = async (): Promise<RandomMedia[]> => await prisma.media.findManyRandom(20, {
+    select: {
+        id: true,
+        isVideo: true,
+        owner: {
+            select: {
+                id: true,
+                name: true,
+                picture: true
+            }
+        }
+    }
+})
