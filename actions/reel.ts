30a7d10d5a1d4d5ebb7313a12744f9b741a05c80
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-11-29 18:09:06 +0200

    Create reel.ts

---
+++ actions/reel.ts
@@ -0,0 +1,42 @@
+import prisma from "@/utils/prisma"
+
+type Reel = {
+    id: string
+    isVideo: boolean
+    owner: {
+        id: string
+        name: string
+        picture: string | null
+    }
+}
+
+const fromReel = (options: any) => ({
+    where: {
+        isVideo: true,
+        ...options
+    },
+    select: {
+        id: true,
+        owner: {
+            select: {
+                id: true,
+                name: true,
+                picture: true,
+            }
+        }
+    }
+})
+
+export const getReel = async (id: string): Promise<Reel | null> => await prisma.media.findUnique(fromReel({
+    id
+}))
+
+export const getReels = async (user: string): Promise<Reel[]> => await prisma.media.findMany(fromReel({
+    ownerId: user,
+}))
+
+export const getRelatedReels = async (reel: string): Promise<Reel[]> => await prisma.media.findManyRandom(10, fromReel({
+    id: {
+        not: reel
+    }
+}))
