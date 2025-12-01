bde8ae8028d5b9d2f15f027424759df5597bb03e
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-01 17:25:31 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,12 @@
+import { existsSync, mkdirSync } from "fs"
 import { homedir } from "os"
 import path from "path"
 
 export const MEDIA_ROOT = process.env.MEDIA_ROOT ?
     path.join(homedir(), process.env.MEDIA_ROOT) :
     path.join(process.cwd(), "media")
+export const THUMB_ROOT = path.join(process.cwd(),"public", "images", "thumbs")
+
+
+if (!existsSync(MEDIA_ROOT)) mkdirSync(MEDIA_ROOT, { recursive: true })
+if (!existsSync(THUMB_ROOT)) mkdirSync(THUMB_ROOT, { recursive: true })
