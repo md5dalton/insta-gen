3a8ddd338b2e7f06d0ea32995ff6896c914625a6
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-01 20:13:19 +0200

    Update route.ts

--- app/api/feed/[page]/route.ts
+++ app/api/feed/[page]/route.ts
@@ -1,11 +1,45 @@
 import { getRandom, MediaResponse } from "@/actions/media"
 import { NextResponse } from "next/server"
+export const runtime = "nodejs"
 
-export const GET = async (req, { params }): Promise<NextResponse> => {
+type Person = {
+    id: string
+    name: string
+    picture: string | null
+}
+type Media = {
+    id: string
+    isVideo: boolean
+    metadata: {
+        width: number
+        height: number
+    }
+}
+
+type Post = {
+    id: string
+    uid: string
+    owner: Person
+    aspect: number
+    media: Media[]
+}
+export const GET = async (req, { params }): Promise<NextResponse<Post[]>> => {
 
     const { page } = await params
 
     const media = await getRandom()
 
-    return NextResponse.json(media.map((m: MediaResponse) => ({...m, uid: `${page}:${m.id}`})))
+    return NextResponse.json(media.map((m: MediaResponse) => ({
+        id: m.id,
+        uid: `${page}:${m.id}`,
+        owner: m.owner,
+        aspect: m.metadata.height/m.metadata.width,
+        media: [
+            {
+                id: m.id,
+                isVideo: m.isVideo,
+                metadata: m.metadata
+            }
+        ]
+    })))
 }
