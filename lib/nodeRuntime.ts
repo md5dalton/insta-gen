b87b5495eda7ce089e695c505e6dcdb98f84d4ba
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-03 07:39:45 +0200

    Update nodeRuntime.ts

--- lib/nodeRuntime.ts
+++ lib/nodeRuntime.ts
@@ -1,5 +1,12 @@
-import { homedir } from "os"
-import path from "path"
 
-export const relativePath = (absolutePath: string): string => absolutePath.replace(homedir(), "")
-export const absolutePath = (relativePath: string): string => path.join(homedir(), relativePath)
+export const generateId = async (path: string) => {
+        
+    const crypto = await import("crypto")
+    
+    const hash = crypto.createHash("sha256").update(path).digest("hex")
+
+    const base64Hash = Buffer.from(hash, "hex").toString("base64")
+
+    return base64Hash.replace(/[^a-zA-Z0-9]/g, "").substring(0, 8)
+        
+}
