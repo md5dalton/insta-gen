743a3332e5230bc14a70062442f3fb4d3d472ffc
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-06 09:24:36 +0200

    Update services.ts

--- services/services.ts
+++ services/services.ts
@@ -9,22 +9,6 @@ declare global {
     var servicesInitialized: boolean
 }
 
-const MEDIA_ROOT = process.env.MEDIA_ROOT
-
-const mediaLibraryPath = MEDIA_ROOT ? path.join(homedir(), MEDIA_ROOT) : path.join(process.cwd(), "media-library")
-
-const watchFolders = (path: string, type: MediaType) => {
-
-    if (!fs.existsSync(path)) {
-        console.log(`Creating media library directory at: ${path}`)
-        fs.mkdirSync(path, { recursive: true })
-    }
-
-    scannerServiceInstance.startScanner(path, type)
-    console.log("Services initialized and scanner started.")
-    console.log("You can now add video files to the " + path + " folder in your project root.")
-}
-
 export const initializeServices = () => {
     if (process.env.NODE_ENV === "development") {
         if (global.servicesInitialized) return
