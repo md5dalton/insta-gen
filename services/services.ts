b2b80821c9120508607576b8a5757881a11ff3e8
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-07 21:06:32 +0200

    Update services.ts

--- services/services.ts
+++ services/services.ts
@@ -1,27 +1,18 @@
-import * as path from "path"
-import * as fs from "fs"
-import { MediaType } from "@/types/type"
-import { scannerServiceInstance } from "./scanner.service"
-import { homedir } from "os"
+import { mediaProcessorInstance } from "./scanner.service"
 
 // This is a workaround to ensure the scanner runs only once in dev mode with hot-reloading
 declare global {
     var servicesInitialized: boolean
 }
 
-export const initializeServices = () => {
+export const initializeServices = async () => {
     if (process.env.NODE_ENV === "development") {
         if (global.servicesInitialized) return
         
         global.servicesInitialized = true
     }
   
-    console.log("Initializing services...")
-
-    const MOVIE_DIR = path.join(mediaLibraryPath, "Movie")
-    const TV_DIR = path.join(mediaLibraryPath, "TV")
-
-    console.log(homedir())
-    watchFolders(MOVIE_DIR, MediaType.Movie)
-    watchFolders(TV_DIR, MediaType.TV)
+    global.mediaProcessor = mediaProcessorInstance
+    await global.mediaProcessor.initialize()
+            
 }
