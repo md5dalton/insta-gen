482c05f61386e9b6028250902b7f8c8ad624c6fe
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-12-14 11:51:56 +0200

    Update instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,33 +1,37 @@
-// import { mediaProcessorInstance } from "./services/media.processor"
-import { initializeServices } from "./services/services"
+import DebouncedMediaProcessor from "./services/scanner.service"
+import { getMediaRoot } from "./config/media"
 
 export async function register(): Promise<void> {
+
     if (process.env.NEXT_RUNTIME === "nodejs") {
         console.log("🚀 Initializing media processor via instrumentation...")
 
-        initializeServices()
-        // try {
-        
-        //     console.log("✅ Media processor initialized via instrumentation")
+        global.mediaProcessor = new DebouncedMediaProcessor(getMediaRoot())
+
+        try {
             
-        //     // Handle graceful shutdown
-        //     process.on("SIGTERM", async () => {
-        //         console.log("🛑 Received SIGTERM, shutting down media processor...")
-
-        //         if (global.mediaProcessor) await global.mediaProcessor.dispose()
-                
-        //         process.exit(0)
-        //     })
-
-        //     process.on("SIGINT", async () => {
-        //         console.log("🛑 Received SIGINT, shutting down media processor...")
-        //         if (global.mediaProcessor) await global.mediaProcessor.dispose()
-                    
-        //         process.exit(0)
-        //     })
+            await global.mediaProcessor.initialize()
         
-        // } catch (error) {
-        //     console.error("❌ Failed to initialize media processor:", error)
-        // }
+            console.log("✅ Media processor initialized via instrumentation")
+            
+            // Handle graceful shutdown
+            process.on("SIGTERM", async () => {
+                console.log("🛑 Received SIGTERM, shutting down media processor...")
+
+                if (global.mediaProcessor) await global.mediaProcessor.dispose()
+                
+                process.exit(0)
+            })
+
+            process.on("SIGINT", async () => {
+                console.log("🛑 Received SIGINT, shutting down media processor...")
+                if (global.mediaProcessor) await global.mediaProcessor.dispose()
+                    
+                process.exit(0)
+            })
+        
+        } catch (error) {
+            console.error("❌ Failed to initialize media processor:", error)
+        }
     }
 }
