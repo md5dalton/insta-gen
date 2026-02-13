ece795b56db99f9636b3fe01d492833ad51f0b59
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-02-13 10:15:07 +0200

    Update reel.ts

--- actions/reel.ts
+++ actions/reel.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma"
 import { MediaType } from "@/types/type"
 
-type Reel = {
+export type Reel = {
     id: string
     isVideo: boolean
     owner: {
@@ -32,11 +32,25 @@ export const getReel = async (id: string): Promise<Reel | null> => await prisma.
     id
 }))
 
+export const getUserReels = async (user: string, cursor: string, skip: number, count: number): Promise<Reel[]> => await prisma.media.findMany({
+    take: count,
+    skip,
+    cursor: {
+        id: cursor
+    },
+    orderBy: {
+        createdAt: "asc",
+    },
+    ...fromReel({
+        ownerId: user,
+    })
+})
+
 export const getReels = async (user: string): Promise<Reel[]> => await prisma.media.findMany(fromReel({
     ownerId: user,
 }))
 
-export const getRelatedReels = async (reel: string): Promise<Reel[]> => await prisma.media.findManyRandom(10, fromReel({
+export const getRelatedReels = async (reel: string, count: number): Promise<Reel[]> => await prisma.media.findManyRandom(count, fromReel({
     id: {
         not: reel
     }
