06ccbeec3b13a1b75d39fbb64205d2322fd776e5
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-03-10 22:53:19 +0200

    Update route.ts

--- app/api/feed/[page]/route.ts
+++ app/api/feed/[page]/route.ts
@@ -1,51 +1,18 @@
 import { getRandom, MediaResponse } from "@/actions/media"
-import { MediaType, ParamsPage } from "@/types/type"
+import { ParamsPage } from "@/types/type"
 import { NextRequest } from "next/server"
-export const runtime = "nodejs"
 
-type Person = {
-    id: string
-    name: string
-    picture: string | null
-}
-type Media = {
-    id: string
-    isVideo: boolean
-    metadata: {
-        width: number
-        height: number
-    }
-}
-
-type Post = {
-    id: string
-    uid: string
-    owner: Person
-    aspect: number
-    media: Media[]
-}
 export const GET = async (req: NextRequest, { params }: ParamsPage) => {
 
     const { page } = await params
 
     const media = await getRandom()
 
-    const res = media.map((m: MediaResponse) => ({
-        id: m.id,
-        uid: `${page}:${m.id}`,
-        owner: m.owner,
-        aspect: m.height/m.width,
-        media: [
-            {
-                id: m.id,
-                isVideo: m.type === MediaType.VIDEO,
-                metadata: {
-                    height: m.height,
-                    width: m.width
-                }
-            }
-        ]
+    const res = media.map(({ type, ...rest }: MediaResponse) => ({
+        ...rest,
+        uid: `${page}:${rest.id}`,
+        mediaType: type
     }))
-
+    
     return Response.json(res)
 }
