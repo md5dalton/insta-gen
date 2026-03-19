cdd7c3cc59d8cef43e15672c4be435a94b6f7fa5
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-03-19 14:39:19 +0200

    Add user stats

--- actions/user.ts
+++ actions/user.ts
@@ -1,30 +1,25 @@
 import prisma from "@/lib/prisma"
+import { User } from "@/prisma/generated/client"
 import { MediaType } from "@/types/type"
 
 type Post = {
     id: string
 }
 
-export const getUser = async (id: string) => await prisma.user.findUnique({
+export const getUser = async (id: string): Promise<Pick<User, "id" | "name" | "picture"> | null> => await prisma.user.findUnique({
     where: { id },
     select: {
         id: true,
         name: true,
         picture: true,
-        _count: {
-            select: {
-                media: true
-            }
-        }
     }
 })
 
 export const getUserStats = async (id: string) => await prisma.media.groupBy({
     by: ["type"],
     where: { ownerId: id },
-    _count: { _all: true }
+    _count: { type: true }
 })
-
 
 export const getPosts = async (id: string, type: MediaType, count: number, skip: number): Promise<Post[]> => await prisma.media.findMany({
     where: {
--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,4 +1,5 @@
-import { getUser } from "@/actions/user"
+import { getUser, getUserStats } from "@/actions/user"
+import { MediaType } from "@/prisma/generated/enums"
 import { ParamsId } from "@/types/type"
 import { NextRequest } from "next/server"
 
@@ -10,9 +11,23 @@ export const GET = async (req: NextRequest, { params }: ParamsId) => {
 
     if (!user) return new Response("User not found", { status: 404 })
 
+    const stats = await getUserStats(user.id)
+
+    const counts = {
+        posts: 0,
+        reels: 0,
+    }
+
+    for (const item of stats) {
+        if (item.type === MediaType.IMAGE) counts.posts = item._count.type
+        if (item.type === MediaType.VIDEO) counts.reels = item._count.type
+    }
+
     return Response.json({
         id: user.id,
         name: user.name,
-        picture: user.picture
+        picture: user.picture,
+        user,
+        stats: counts
     })
 }
