af209a439c52f2afb21646cf18414cca0dbd9982
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-03-22 15:26:06 +0200

    Update explore.ts

--- actions/explore.ts
+++ actions/explore.ts
@@ -1,15 +1,17 @@
 import prisma from "@/lib/prisma"
-import { MediaType } from "@/types/type"
+import { MediaType } from "@/prisma/generated/enums"
 
 export type MediaResponse = {
     id: string
+    ownerId: string
     type: MediaType
 }
 
 export const getRandom = async (count: number, type: MediaType): Promise<MediaResponse[]> => await prisma.$queryRaw`
     SELECT 
         m.id,
-        m.type
+        m.type,
+        m."ownerId"
     FROM "Media" m
     WHERE m.type = ${type}
     ORDER BY RANDOM()
