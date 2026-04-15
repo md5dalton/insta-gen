4f612d2217a7dbe595cc63435c69ab4ff9eb0b89
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-04-15 15:17:26 +0200

    Update route.ts

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getUser, getUserStats } from "@/actions/user"
+import { getTagCount, getUser, getUserStats } from "@/actions/user"
 import { MediaType } from "@/prisma/generated/enums"
 import { ParamsId } from "@/types/type"
 import { NextRequest } from "next/server"
@@ -12,10 +12,12 @@ export const GET = async (req: NextRequest, { params }: ParamsId) => {
     if (!user) return new Response("User not found", { status: 404 })
 
     const stats = await getUserStats(user.id)
+    const tagsCount = await getTagCount(user.id)
 
     const counts = {
         posts: 0,
         reels: 0,
+        tags: tagsCount,
     }
 
     for (const item of stats) {
