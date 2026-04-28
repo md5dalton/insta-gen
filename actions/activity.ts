6b87d5dc63a16225c12364307a23644efaaa92ca
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-04-28 23:18:27 +0200

    Update activity.ts

--- actions/activity.ts
+++ actions/activity.ts
@@ -8,3 +8,26 @@ export const likePost = async (userId: string, mediaId: string) => await prisma.
 export const unLikePost = async (userId: string, mediaId: string) => await prisma.like.delete({
     where: { userId_mediaId: { userId, mediaId } }
 })
+export async function toggleLike(userId: string, mediaId: string) {
+    try {
+        // Try to create (like)
+        await prisma.like.create({
+            data: { userId, mediaId },
+        })
+
+        return true
+    } catch (error: any) {
+        // If it already exists → unlike
+        if (error.code === "P2002") {
+            await prisma.like.delete({
+                where: {
+                    userId_mediaId: { userId, mediaId },
+                },
+            })
+
+            return false
+        }
+
+        throw error
+    }
+}
