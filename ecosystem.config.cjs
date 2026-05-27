b42ba851f17aa9c3147f1746b185748da6e4e1eb
LesliMD <90160065+md5dalton@users.noreply.github.com>
2026-05-27 12:58:44 +0200

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+    apps: [
+        {
+            name: "nextjs",
+            cwd: "/home/app/app",
+
+            script: "npm",
+            args: "start",
+
+            env: {
+                NODE_ENV: "production",
+                PORT: 4000
+            },
+
+            instances: 1,
+            exec_mode: "fork",
+
+            autorestart: true,
+            watch: false,
+
+            max_memory_restart: "1G",
+
+            error_file: "/home/app/.pm2/logs/nextjs-error.log",
+            out_file: "/home/app/.pm2/logs/nextjs-out.log",
+            log_file: "/home/app/.pm2/logs/nextjs-combined.log",
+
+            time: true
+        },
+
+        {
+            name: "watcher",
+            cwd: "/home/app/app",
+
+            script: "npm",
+            args: "run watcher",
+
+            env: {
+                NODE_ENV: "production"
+            },
+
+            instances: 1,
+            exec_mode: "fork",
+
+            autorestart: true,
+            watch: false,
+
+            max_memory_restart: "512M",
+
+            error_file: "/home/app/.pm2/logs/watcher-error.log",
+            out_file: "/home/app/.pm2/logs/watcher-out.log",
+            log_file: "/home/app/.pm2/logs/watcher-combined.log",
+
+            time: true
+        }
+    ]
+}
