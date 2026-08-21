399f8cad3d1745bd681fbd27e8c77fc19c8f5e3b
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2026-08-21 18:38:29 +0200

    Create route.ts

---
+++ app/api/media/thumb/[slug]/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server"
+import { Storage } from "@/lib/storage"
+import { MediaConfig } from "@/lib/config"
+
+const storage = new Storage(MediaConfig.ASSETS_ROOT)
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
+    const { slug } = await params
+
+    let [id, isVideo] = slug.split(":")
+
+    const imagePath = (isVideo ? "videos" : "images") +  `/${id}/` + (isVideo ? "poster" : "thumb") + ".webp"
+
+    if (!(await storage.exists(imagePath))) return new Response("Thumbnail not found", { status: 404 })
+
+    const stream = await storage.stream(imagePath)
+
+    return new Response(stream as unknown as ReadableStream, {
+        headers: {
+            "Content-Type": "image/webp",
+            "Cache-Control": "public, max-age=31536000, immutable",
+            ETag: slug,
+        },
+    })
+}
